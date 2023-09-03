8cfd14d894590e0b8de7fae264b3c41301ea7bb4
Federico <federeyes2000@gmail.com>
2023-09-03 18:05:47 -0300

    clase 1

---
+++ Pages/Profesor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clase1.Pages
+{
+    public class ProfesorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
